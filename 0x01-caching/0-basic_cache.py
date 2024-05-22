7078db12237de32f485d7c89d48f31838b459d89
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-22 14:24:33 +0300

    Task 1

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Task 1"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """inherits from BaseCaching and is a caching system"""
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data
+        the item value for the key key"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key"""
+        if key:
+            try:
+                return self.cache_data[key]
+            except KeyError:
+                pass
+
+        return None
