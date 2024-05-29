d3be288870faf85cf045136db8ca4139720a53e2
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-29 12:40:12 +0300

    Now???

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,38 +1,33 @@
 #!/usr/bin/env python3
-""" Module for Second Flask Task """
-
-from typing import Optional
+""" 2. get local from request """
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
-class Config():
-    """a Config class that has a LANGUAGES class attribute
-    equal to ["en", "fr"]"""
+class Config:
+    """ Config class for app """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@app.route('/', strict_slashes=False)
-def home_route() -> str:
-    """simply outputs “Welcome to Holberton” as page title (<title>)
-    and “Hello world” as header (<h1>)
-    """
+@babel.localeselector
+def get_locale() -> str:
+    """ Get locale from request """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def welcome() -> str:
+    """ / page """
     return render_template('3-index.html')
 
 
-@babel.localeselector
-def get_locale() -> Optional[str]:
-    """determine the best match with our supported languages"""
-    return request.accept_languages.best_match(Config.LANGUAGES)
-
-
 if __name__ == '__main__':
-    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
